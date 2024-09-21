674a555850cce096a41a5f578b11a455eb8b5cbf
Marcos Oliveira <mspol23@gmail.com>
2024-09-21 18:09:39 -0300

    implement SellerDaoJDBC and DaoFactory

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,8 @@ package main;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,7 @@ public class Main {
 		
 		Department obj = new Department(1, "Books");
 		Seller seller = new Seller(21, "John Doe", "john@gmail.com", new Date(), 1000.0, obj);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		System.out.println(seller);
 	}
 
