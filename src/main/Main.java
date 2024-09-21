ee65f6ee5d2ff82436ef3d5c28b6a64a4ffe32f0
Marcos Oliveira <mspol23@gmail.com>
2024-09-21 14:51:14 -0300

    implement Seller class

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,13 +1,17 @@
 package main;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		Seller seller = new Seller(21, "John Doe", "john@gmail.com", new Date(), 1000.0, obj);
+		System.out.println(seller);
 	}
 
 }
